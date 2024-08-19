2c6d2c640cf1c76d28fb6e75ee5114c336809d0b
jonatha23456789 <159666966+jonatha23456789@users.noreply.github.com>
2024-08-19 06:41:42 +0100

    Update and rename uptime3.js to uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
-    name: "uptime3",
-    aliases: ["up3", "upt3"],
+    name: "uptime",
+    aliases: ["upt", "upt"],
     version: "1.0",
     author: "XyryllPanget",
     role: 0,
@@ -23,6 +23,6 @@ module.exports = {
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-    api.sendMessage(`🧑‍🏫|𝙃𝙄 𝙈𝘼𝙎𝙏𝙀𝙍📑 \n___________________\n, 🎀 𝙎𝘼𝙆𝙐𝙍𝘼 𝘼𝙎 𝘽𝙀𝙀𝙉 𝙍𝙐𝙉𝙄𝙉𝙂 𝙁𝙊𝙍 \n____________________\n [⏰${uptimeString}]\n\n______________________🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼🧛.`, event.threadID);
+    api.sendMessage(`🧑‍🏫|𝙃𝙄 𝙈𝘼𝙎𝙏𝙀𝙍 𝗛𝗜𝗞𝗔𝗥𝗨📑 \n___________________\n, 🎀 𝗟𝗢𝗩𝗘𝗟𝗬 𝗛𝘼𝙎 𝘽𝙀𝙀𝙉 𝙍𝙐𝙉𝙄𝙉𝙂 𝙁𝙊𝙍 \n____________________\n [⏰${uptimeString}]\n\n______________________🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼🧛.`, event.threadID);
   }
 };
