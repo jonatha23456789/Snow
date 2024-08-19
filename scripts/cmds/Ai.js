c56ab1908f7fa361095bee7a6a657814eae01626
jonatha23456789 <159666966+jonatha23456789@users.noreply.github.com>
2024-08-19 06:34:37 +0100

    Update Ai.js

--- scripts/cmds/Ai.js
+++ scripts/cmds/Ai.js
@@ -12,13 +12,10 @@ async function fetchFromAI(url, params) {
 
 async function getAIResponse(input, userId, messageID) {
   const services = [
-    { url: 'https://ai-tools.replit.app/gpt', params: { prompt: input, uid: userId } },
-    { url: 'https://openaikey-x20f.onrender.com/api', params: { prompt: input } },
-    { url: 'http://fi1.bot-hosting.net:6518/gpt', params: { query: input } },
-    { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+     { url: 'https://metoushela-rest-api-tp5g.onrender.com/api/gpt4o?', params: { context: input } }
   ];
 
-  let response = "[😉] 𝗛𝗘𝗟𝗟𝗢 𝗜 𝗔𝗠 𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜 👑👸 𝗔𝗦𝗞 𝗠𝗘 𝗔𝗡𝗬𝗧𝗛𝗜𝗡𝗚 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗕𝗬 𝗞𝗘𝗟𝗩𝗜𝗡 𝗛𝗜𝗞𝗔𝗥𝗨";
+  let response = "Hey salut 👋🏾👸! Belle journée, pas vrai ? Pose ta question 💭, je serai ravie de t'aider.💜";
   let currentIndex = 0;
 
   for (let i = 0; i < services.length; i++) {
@@ -37,7 +34,7 @@ async function getAIResponse(input, userId, messageID) {
 module.exports = {
   config: {
     name: 'ai',
-    author: 'Arn',
+    author: 'Metoushela',
     role: 0,
     category: 'ai',
     shortDescription: 'ai to ask anything',
@@ -45,19 +42,24 @@ module.exports = {
   onStart: async function ({ api, event, args }) {
     const input = args.join(' ').trim();
     if (!input) {
-      api.sendMessage(`𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜 👑👸\n✿✿✿✿✿✿✿✿✿✿✿✿\nPlease provide a question or statement.\n━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
+      api.sendMessage(`📑 𝙿𝚕𝚎𝚊𝚜𝚎 𝚙𝚛𝚘𝚟𝚒𝚍𝚎 a 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗 𝚘𝚛 𝚜𝚝𝚊𝚝𝚎𝚖𝚎𝚗𝚝. `, event.threadID, event.messageID);
       return;
     }
 
     const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
-    api.sendMessage(`𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜 👑👸 \n✿✿✿✿✿✿✿✿✿✿✿✿\n${response}\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+    api.sendMessage(` 🎀...............................\n¥n${response}\n\n🎀...............................`, event.threadID, messageID);
   },
   onChat: async function ({ event, message }) {
     const messageContent = event.body.trim().toLowerCase();
     if (messageContent.startsWith("ai")) {
       const input = messageContent.replace(/^ai\s*/, "").trim();
       const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
-      message.reply(`𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜 👑👸\n✿✿✿✿✿✿✿✿✿✿✿✿\n${response}\n━━━━━━━━━━━━━━━━`, messageID);
+      message.reply(`
+        
+👑𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜  | 👸
+━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━\n 🌸👑𝗟𝗢𝗩𝗘𝗟𝗬 𝗔𝗜 👑🌸\n━━━━━━━━━━━━━━━
+
+`, messageID);
     }
   }
 };
